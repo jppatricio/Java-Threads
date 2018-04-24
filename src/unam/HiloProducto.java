8779174eaf3d45af1b39ba9d0e28f0b68cd733d8
Patricio JP <jppatricio96@gmail.com>
2018-04-24 02:28:00 -0500

    Impl Multiplicacion
    Fatla agregar transposicion y hacer que los hilos se sincroniz.

---
+++ src/unam/HiloProducto.java
@@ -0,0 +1,34 @@
+package unam;
+
+
+public class HiloProducto extends Thread{
+
+	public String nombre;
+	public int[] x;
+	public int[][] y;
+	public int fila;
+	
+	public HiloProducto(String nombre, int[] x, int[][] y, int fila) {
+		this.nombre = nombre;
+		this.x = x;
+		this.y = y;
+		this.fila = fila;
+	}
+	
+	@Override
+	public void run(){
+//		System.out.println("X:" + x.length + " Y: " + y.length); Prueba de que siempre son iguales
+		for(int i = 0; i < Main.cB; i++)
+		{
+			int resultParcial = 0;
+			
+			for(int j = 0; j < x.length; j++){
+				resultParcial = resultParcial + (x[j]*y[j][i]);
+			}
+			
+			Main.c[fila][i] = resultParcial;
+			
+			System.out.println(nombre + ": " + "C[" + fila + "]" + "[" + i + "] = " + resultParcial);
+		}
+	}
+}
--- src/unam/Main.java
+++ src/unam/Main.java
@@ -9,6 +9,9 @@ public class Main {
 	public static Integer rA, rB, cA, cB; // Renglones y columnas de ambas matrices
 	public static int[][] a; // Hacemos los arreglos globales para poder llamarlos desde los hilos
 	public static int[][] b;
+
+	public static int[][] c; // el arreglo del resultado final
+
 	 
 	public static void main(String[] args) {
 
@@ -46,7 +49,28 @@ public class Main {
 				colB ++;
 			}
 		}while (ia < rA | ib < rB);
+		//AQUI EMPIEZA LA TRANSPOSICION!!!!!!!!!!!!!!!vvvvvvv
+	
+		
+		//AQUI EMPIEZA LA MULTIPLICACION (HAY QUE MODIFICAR rA rB cA y cB a los transpuestos)
+		// Y SI PUEDES, HAY QUE HACER QUE NO EMPIEZE LA TRASPOSICION HASTA QUE ACABEN LOS HILOS DE ARRIBA
+		// Y QUE NO EMPIECE LA MULT HASTA QUE ACABE LOS HILOS DE TRANSPOC.
+		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!vvvvvv
+	c = new int[rA][cB];
+	
+	int[] x = new int[cA];// los arreglos a mandar para el producto x= datos de cada fila de A
+
+	for(int i1 = 0; i1< rA; i1++){
+		for(int j= 0; j<cA; j++){ // los datos de la fila x de a
+			x[j] = a[i1][j];
+		}
+			new HiloProducto("HiloProd " + i1 ,x,b,i1).run();
+		}
 	}
+	
+	
+	
+	//--
 	
 	public static Boolean IngresoDatos(){
 		
