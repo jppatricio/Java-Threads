8db4b5ae02d8e878302ca591c788c58c318a36cb
Patricio JP <jppatricio96@gmail.com>
2018-04-23 23:31:00 -0500

    Falla El Hilo Llenado
    Regresa NullException en el arreglo Main.a y Main.b

---
+++ src/unam/HiloLlenadoArreglo.java
@@ -0,0 +1,39 @@
+package unam;
+
+import java.util.Random;
+
+public class HiloLlenadoArreglo extends Thread{
+	
+	private String nombre;
+	private String arr;
+	private int row;
+	
+	 public HiloLlenadoArreglo(String nombre, String arr, int row) {
+		 this.nombre = nombre;
+		 this.arr = arr;
+		 this.row = row;
+	}
+	
+	public void run() {
+		if(arr == "A")
+		{
+			
+			for(int i = 0; i < Main.cA; i ++)
+			{
+				System.out.println(Main.a.toString());
+				Main.a[row][i] =  new Random().nextInt(11); //(Va del 0 al 10 excluye al 11)
+				System.out.println(nombre + ": " + "A[" + row + "]" + "[" + i + "] = " + Main.a[row][i]);
+			}
+		}
+		else
+		{
+			for(int i = 0; i < Main.cA; i ++)
+			{
+			Main.b[row][i] =  new Random().nextInt(11); //(Va del 0 al 10 excluye al 11)
+
+			System.out.println(nombre + ": " + "B[" + row + "]" + "[" + i + "] = " + Main.b[row][i]);
+			}
+		}
+    }
+	
+}
--- src/unam/Main.java
+++ src/unam/Main.java
@@ -1,10 +1,76 @@
 package unam;
 
+
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static Integer rA, rB, cA, cB; // Renglones y columnas de ambas matrices
+	public static int[][] a; // Hacemos los arreglos globales para poder llamarlos desde los hilos
+	public static int[][] b;
+	 
+	public static void main(String[] args) {
+
+		Boolean error; // manejo de error
+		do
+		{
+			error = IngresoDatos();
+		
+			if (!error && cA != rB){
+
+				System.out.println("Datos no válidos (no es posible realizar la operación...) \n\n");
+			}
+		
+		}while(error == true);
+		System.out.println("A("+ rA + "x" + cA  + ") y B(" + rB + "x" + cB + ")");
+		
+		 int[][] a = new int [rA][cA];
+		 int[][] b = new int [rB][cB];
+		 int i = 1, colA = 0, colB = 0;
+		do{
+			if (!(i >= cA))
+			{
+				new HiloLlenadoArreglo("Hilo" + i, "A", colA).start();// Llena A
+				i++;
+				colA++;
+			}
+			if(!(i >= cB))
+			{
+				new HiloLlenadoArreglo("Hilo" + i, "B", colB).start();// Llena B
+				i++;
+				colB ++;
+			}
+		}while (a.length != cA && b.length != cB);
+	}
+	
+	public static Boolean IngresoDatos(){
+		
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		
+		try{
+		System.out.println("Número de renglones de la matriz A: (Enteros > 0)\n");
+		rA = Integer.parseInt(sc.nextLine());
+		System.out.println("Número de columnas de la matriz A: (Enteros > 0)\n");
+		cA = Integer.parseInt(sc.nextLine());
+		System.out.println("Número de renglones de la matriz B: (Enteros > 0)\n");
+		rB = Integer.parseInt(sc.nextLine());
+		System.out.println("Número de columnas de la matriz B: (Enteros > 0)\n");
+		cB = Integer.parseInt(sc.nextLine());
+		}
+		catch(Exception e){
+			
+			System.out.println("Datos no válidos (!) \n\n");
+			return true; // Hubo error
+			
+		}
+		if(rA <= 0 | rB <= 0 | cA <= 0 | cB <= 0)
+		{
+			System.out.println("Datos no válidos (Ningun valor puede ser <= 0) \n\n");
+			return true; // Hubo error
+		}
+		return false; // No hubo error
 	}
 
 }
